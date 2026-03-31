582ed594ad816f53004a4dda8fdcb64f2c4b2556
Derek Weiss <derekjweiss08@gmail.com>
2026-03-30 19:59:56 -0700

    fixed the bug with client and server ink a loop also finished up some code in the sync fuction which now should be working correctly and started working on the GUI

--- main.c
+++ main.c
@@ -41,10 +41,24 @@ struct request {
 void sendmessage(int socketFD) {
     struct request request;
     request.RequestType = MESSAGE;
-    printf("\n userName: ");
-    scanf("%s", &(request.userName));
-    printf("\n Message: ");
-    scanf("%s", &(request.Message));
+    request.Message[0] = 'h';
+    request.Message[1] = 'e';
+    request.Message[2] = 'l';
+    request.Message[3] = 'l';
+    request.Message[4] = 'o';
+    request.Message[5] = ' ';
+    request.Message[6] = 'w';
+    request.Message[7] = 'o';
+    request.Message[8] = 'r';
+    request.Message[9] = 'l';
+    request.Message[10] = 'd';
+    request.Message[11] = '\0';
+    request.userName[0] = 'd';
+    request.userName[1] = 'e';
+    request.userName[2] = 'r';
+    request.userName[3] = 'e';
+    request.userName[4] = 'k';
+    request.userName[5] = '\0';
     send(socketFD, &request, sizeof(request),  0);
 }
 void disconnect(int socketFD) {
@@ -52,17 +66,70 @@ void disconnect(int socketFD) {
     request.RequestType = EXIT;
     send(socketFD, &request, sizeof(request), 0);
 }
-struct messageStruct* getsyncdata(int socketFD) {
+struct messageStruct* getsyncdata(int socketFD, int* datasize) {
     struct request request;
     request.RequestType = SNYC;
-    int datasize;
     send(socketFD, &request, sizeof(request), 0);
-    recv(socketFD, &datasize, sizeof(int), 0);
-    struct messageStruct* messages = (struct messageStruct*)malloc(sizeof(struct messageStruct) * datasize);
-    recv(socketFD, messages, sizeof(struct messageStruct) * datasize, 0);
+    recv(socketFD, datasize, sizeof(int), 0);
+    if (*datasize == 0) {
+        return NULL;
+    }
+    struct messageStruct* messages = (struct messageStruct*)malloc(sizeof(struct messageStruct) * *datasize);
+
+    recv(socketFD, messages, sizeof(struct messageStruct) * *datasize, 0);
     return messages;
 }
-void syncdata(int socketFD, struct messageNode** ptail) {
+struct messageNode* CreateMessageNode(struct messageStruct* input) {
+    struct messageNode* node = (struct messageNode*)malloc(sizeof(struct messageNode));
+    node->next = NULL;
+    strcpy(node->userName, input->userName);
+    strcpy(node->Message, input->Message);
+    return node;
+}
+void syncdata(int socketFD, struct messageNode** ptail, struct messageNode** phead) {
+    int datasize = 0;
+    int startpos = 0;
+    struct messageStruct* messages = getsyncdata(socketFD, &datasize);
+    if (datasize == 0) {
+        return;
+    }
+    if (!*ptail) {
+        *ptail = CreateMessageNode(&(messages[0]));
+        *phead = *ptail;
+    }
+    struct messageNode* pnode = *ptail;
+    for (int i = startpos; i < datasize - 1; i++) {
+        pnode->next = CreateMessageNode(&(messages[i]));
+        pnode = pnode->next;
+    }
+    free(messages);
+}
+void printlist(struct messageNode** phead) {
+    struct messageNode* pnode = *phead;
+    while (pnode != NULL) {
+        printf("userName: %s Message: %s\n", pnode->userName, pnode->Message);
+        pnode = pnode->next;
+    }
+}
+void window(int socketFD, struct messageNode** ptail, struct messageNode** phead) {
+    InitWindow(800, 1000, "TextBox example");
+
+    char text[64] = "";
+    bool editMode = false;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Click to activate, click away to deactivate
+        if (GuiTextBox((Rectangle){ 100, 100, 200, 30 }, text, 64, editMode))
+            editMode = !editMode;
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
 }
 int main(void) {
@@ -72,13 +139,17 @@ int main(void) {
     ServerAddr.sin_family = AF_INET;
     ServerAddr.sin_addr.s_addr = inet_addr(IP);
     ServerAddr.sin_port = htons(PORT);
-    connect(socketFD, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
+
+
     struct messageNode* phead = NULL;
     struct messageNode* ptail = NULL;
+    /*
+    while (connect(socketFD, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr)));
 
-
-
+    printlist(&phead);
     disconnect(socketFD);
+    */
+    window(socketFD, &phead, &ptail);
 
 
 }
